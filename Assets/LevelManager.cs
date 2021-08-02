b159abe745eed33dc1e2ef1ffdc488b385fba845
DeepikaVasam123 <deepika.vasam@xcubelabs.com>
2021-08-02 14:25:09 -0700

    Menu scene, options, volume control with playerprefs

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void PreviousLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadPreviousNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
